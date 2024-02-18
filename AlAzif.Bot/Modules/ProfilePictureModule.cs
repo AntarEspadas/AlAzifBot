9f0c3e60859d086e5771ed2092b82327da198eaa
Antar Espadas <antar.espadas@hotmail.com>
2024-02-17 18:09:14 -0600

    Implement pfp module

--- AlAzif.Bot/Modules/ProfilePictureModule.cs
+++ AlAzif.Bot/Modules/ProfilePictureModule.cs
@@ -9,9 +9,15 @@ public class ProfilePictureModule : ApplicationCommandModule
     [SlashCommand("pfp", "Get the profile picture of a user")]
     public async Task ProfilePictureCommand(
         InteractionContext ctx,
-        [Option("user", "The user to get the profile picture of")] string user
+        [Option("user", "The user to get the profile picture of")] DiscordUser user
     )
     {
-        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(user));
+        var response = new DiscordInteractionResponseBuilder()
+            .AddEmbed(
+                new DiscordEmbedBuilder()
+                    .WithImageUrl(user.AvatarUrl)
+                    .WithTitle(user.Username)
+                );
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, response);
     }
 }
